dafa872a6c65e0c40cf8f7e7b694b9a0b593bb22
AbdRidene <abd.ridene@gmail.com>
2026-02-19 12:18:29 +0100

    fix yml file

--- src/main/java/com/sipacademy/stockmanager/configuration/PasswordConfig.java
+++ src/main/java/com/sipacademy/stockmanager/configuration/PasswordConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class PasswordConfig {
@@ -12,11 +12,5 @@ public class PasswordConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    // Vérifie que le PasswordEncoder est bien chargé sur Azure
-    @Bean
-    public CommandLineRunner testPasswordEncoder(PasswordEncoder encoder) {
-        return args -> System.out.println("PasswordEncoder is loaded: " + encoder.encode("test"));
     }
 }
